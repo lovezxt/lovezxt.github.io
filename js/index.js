03ee7a4cba8cd3c439233970ccb33aa2417f6757
高占江 <2463394368@qq.com>
2022-12-08 18:47:52 +0800

    add rain

--- js/index.js
+++ js/index.js
@@ -39,6 +39,16 @@ $(function() {
 			"background": "#000"
 		})
 	});
+
+	Rain({
+		speed: [10,100], // 风速范围
+		hasBounce: true, // 是否有回弹效果
+		wind_direction: 230, // 风向
+		gravity: 0.163, // 重力
+		maxNum: 1000, // 雨滴最大数量
+		numLevel: 10, // 每次生成雨滴数量
+		drop_chance: 0.4 // 下雨的概率
+	});
 });
 
 function goTop(){
